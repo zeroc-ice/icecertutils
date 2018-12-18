19ba9b66273f24d2d4ad17d37e0306ab537076d7
Jose <jose@zeroc.com>
2018-12-18 20:43:09 +0100

    Update copyright

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # **********************************************************************
 #
-# Copyright (c) 2015-2018-2018 ZeroC, Inc. All rights reserved.
+# Copyright (c) 2015-present-present ZeroC, Inc. All rights reserved.
 #
 # **********************************************************************
 
